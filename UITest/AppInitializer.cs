b0a74a0e705b1756b268dae45c36ec73ba30ac8c
Mironova Ekaterina <ekmironova123@gmail.com>
2017-11-05 21:49:44 +0200

    Добавьте файлы проекта.

---
+++ UITest/AppInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+        private const string path = @"D:\..\..\..\CalcSmall_App\bin\Debug\CalcSmall_App.CalcSmall_App.apk";
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
---
+++ UITest/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+        private IAuthScreen Auth;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+            Auth = new AndroidAuthScreen();
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        [Category("Tap")]
+        [TestCase("textA")]
+        [TestCase("textB")]
+        [TestCase("textOp")]
+        [TestCase("textRes")]
+        [TestCase("btnRes")]
+        public void AppButton(string id)
+        {
+            app.Tap(id);
+        }
+    }
+}
+
